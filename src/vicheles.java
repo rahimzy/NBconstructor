0e74f8dc43941d326ed81c0a4b4fd53e0a803b7c
Breshna-Rahimzy-Sheridan <rahimzy@sheridancollege.ca>
2019-03-02 00:18:08 -0500

    adding encapsulation

---
+++ src/vicheles.java
@@ -0,0 +1,40 @@
+/*
+ * Breshna Rahimzy
+ * Student ID: 991487145
+ *     SYST10199 - WEB Programming
+ */
+
+
+public class vicheles {
+    private double gollonsOfGas;
+    private int mpg;
+
+    /**
+     * @return the gollonsOfGas
+     */
+    public double getGollonsOfGas() {
+        return gollonsOfGas;
+    }
+
+    /**
+     * @param gollonsOfGas the gollonsOfGas to set
+     */
+    public void setGollonsOfGas(double gollonsOfGas) {
+        this.gollonsOfGas = gollonsOfGas;
+    }
+
+    /**
+     * @return the mpg
+     */
+    public int getMpg() {
+        return mpg;
+    }
+
+    /**
+     * @param mpg the mpg to set
+     */
+    public void setMpg(int mpg) {
+        this.mpg = mpg;
+    }
+    
+}
